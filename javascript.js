20c6c8bfd694a795795b45a839efa93989c95d07
agoofygoat <nicholasrueb@gmail.com>
2026-06-02 08:33:09 -0400

    Add event listener for match prediction logic

---
+++ javascript.js
@@ -0,0 +1,164 @@
+document.getElementById('predictButton').addEventListener('click', async function() {
+    console.log("PROGRAM STARTED!");
+
+    //const defaultClient = TbaApiV3client.ApiClient.instance;
+   // const apiKeyConfig = defaultClient.authentications['apiKey']; this the old stuff
+    const apiKey = "ukqYw6gLRcDFrqFozY4irknvHZwTgB86fkm3fOtPyYrn022u7BFEKVOPV6JSDUjT";
+    const baseUrl = "https://www.thebluealliance.com/api/v3";
+    const eventKey = document.getElementById('eventKeyInput').value;
+    var totalTrue =0;
+    var checkedMatches = 0;
+    const fetchOptions = {
+        method: 'GET',
+        headers: {
+            'X-TBA-Auth-Key': apiKey,
+            'accept': 'application/json' //idek this from gem and the docs, for the new fetch way of getting data
+        }
+    };
+    try {
+        const [matchesResponse, teamsResponse, oprsResponse] = await Promise.all([
+            fetch(`${baseUrl}/event/${eventKey}/matches`, fetchOptions),
+            fetch(`${baseUrl}/event/${eventKey}/teams`, fetchOptions), //actually fetching the array
+            fetch(`${baseUrl}/event/${eventKey}/oprs`, fetchOptions)
+        ]);
+
+        const data = await matchesResponse.json();
+        const teamData = await teamsResponse.json(); //parsing
+        const oprData = await oprsResponse.json();
+
+                const oprs = oprData.oprs || {};
+                const dprs = oprData.dprs || {};  //finally making them into arrays
+                const ccwms = oprData.ccwms || {};
+
+                data.sort((a, b) => a.match_number - b.match_number); //sorting in order
+
+                console.log(`📥 Successfully retrieved matches. Filtering schedule:\n`);
+                console.log("-------------------------------------------------");
+
+                let htmlOutput = "";
+
+                for (let i = 0; i < data.length; i++) {
+                    const currentMatch = data[i];
+
+                    const redAlliance = currentMatch.alliances.red;
+                    const blueAlliance = currentMatch.alliances.blue;
+                    const redTeams = redAlliance.team_keys;
+                    const blueTeams = blueAlliance.team_keys;
+
+                    const red1 = parseInt(redTeams[0].substring(3));
+                    const red2 = parseInt(redTeams[1].substring(3));
+                    const red3 = parseInt(redTeams[2].substring(3));
+                    const blue1 = parseInt(blueTeams[0].substring(3));
+                    const blue2 = parseInt(blueTeams[1].substring(3));
+                    const blue3 = parseInt(blueTeams[2].substring(3));
+
+                    htmlOutput += "Match " + (i + 1) + ":<br>";
+                    htmlOutput += "Red Alliance: " + red1 + ", " + red2 + ", " + red3 + "<br>";
+                    htmlOutput += "Blue Alliance: " + blue1 + ", " + blue2 + ", " + blue3 + "<br>";
+
+                    var totalRedOPR = 0;
+                    var totalBlueOPR = 0;
+                    var totalRedDPR = 0;
+                    var totalBlueDPR = 0;
+                    var highestBlueCCWMName = "frcNull";
+                    var highestRedCCWMName = "frcNull";
+                    var highestBlueCCWM = -Infinity;
+                    var highestRedCCWM = -Infinity;
+                    var totalRedCCWM = 0;
+                    var totalBlueCCWM = 0;
+
+                    var winningAlliance = currentMatch.winning_alliance
+                    if(winningAlliance != "")
+                    {
+                        checkedMatches++;
+                    }
+
+
+                    for (let j = 0; j < teamData.length; j++) {
+                        var currentTeam = teamData[j];
+
+                        if (currentTeam.key === ("frc" + red1) || currentTeam.key === ("frc" + red2) || currentTeam.key === ("frc" + red3)) {
+                            totalRedOPR += (oprs[currentTeam.key]);
+                            totalRedDPR += (dprs[currentTeam.key]);
+                            totalRedCCWM += (ccwms[currentTeam.key]);
+                            if(ccwms[currentTeam.key] > highestRedCCWM) {
+                                highestRedCCWMName = currentTeam.key;
+                                highestRedCCWM = (ccwms[currentTeam.key]);
+                            }
+                        }
+                        if (currentTeam.key === ("frc" + blue1) || currentTeam.key === ("frc" + blue2) || currentTeam.key === ("frc" + blue3)) {
+                            totalBlueOPR += (oprs[currentTeam.key]);
+                            totalBlueDPR += (dprs[currentTeam.key]);
+                            totalBlueCCWM += (ccwms[currentTeam.key]);
+                            if(ccwms[currentTeam.key] > highestBlueCCWM) {
+                               highestBlueCCWMName = currentTeam.key;
+                                highestBlueCCWM = (ccwms[currentTeam.key]);
+                            }
+                        }
+                    }
+
+                    var totalRedAmount = totalRedOPR + totalBlueDPR;
+                    var totalBlueAmount = totalBlueOPR + totalRedDPR;
+                    var predictedWin = "";
+                    if(totalRedAmount > totalBlueAmount)
+                    {
+                        predictedWin = "red"
+                    }
+                    else
+                    {
+                        predictedWin = "blue"
+                    }
+                    var isCorrect = false
+                    if(predictedWin === winningAlliance)
+                    {
+                        isCorrect = true;
+                    }
+                    if(Math.abs(totalRedAmount - totalBlueAmount) < 25) {
+                        if(totalRedCCWM > totalBlueCCWM) {
+                            htmlOutput += "Red Alliance is predicted to have a  " + calcPercent(totalRedAmount, totalBlueAmount, "red") + "% chance of winning<br>"
+                            htmlOutput +=  "Team " + highestRedCCWMName.replace("frc", "") + " has highest Red carry potential.<br>"
+                            htmlOutput += "Team " + highestBlueCCWMName.replace("frc", "") + " has highest Blue carry potential.<br>"
+                        } else {
+                            htmlOutput += "Blue Alliance is predicted to have a  " + calcPercent(totalRedAmount, totalBlueAmount, "blue") + "% chance of winning<br>"
+                            htmlOutput += "Team " + highestRedCCWMName.replace("frc", "") + " has highest Red carry potential.<br>"
+                            htmlOutput += "Team " + highestBlueCCWMName.replace("frc", "") + " has highest Blue carry potential.<br>"
+                        }
+                    } else if(totalBlueAmount > totalRedAmount) {
+                        htmlOutput += "Blue Alliance is predicted to have a  " + calcPercent(totalRedAmount, totalBlueAmount, "blue") + "% chance of winning<br>"
+                        htmlOutput += "Team " + highestRedCCWMName.replace("frc", "") + " has highest Red carry potential.<br>"
+                        htmlOutput += "Team " + highestBlueCCWMName.replace("frc", "") + " has highest Blue carry potential.<br>"
+                    } else {
+                        htmlOutput +=  "Red Alliance is predicted to have a  " + calcPercent(totalRedAmount, totalBlueAmount, "red") + "% chance of winning<br>"
+                        htmlOutput += "Team " + highestRedCCWMName.replace("frc", "") + " has highest Red carry potential.<br>"
+                        htmlOutput += "Team " + highestBlueCCWMName.replace("frc", "") + " has highest Blue carry potential.<br>"
+                    }
+                    //htmlOutput += isCorrect + "</br>"
+                    htmlOutput += "--------------------------------<br>";
+                    if(isCorrect)
+                    {
+                        totalTrue ++
+                    }
+                }
+                var percentCorrect = 100*totalTrue/checkedMatches;
+                percentCorrect = Math.round(percentCorrect * 100.0)/100.0;
+                htmlOutput += percentCorrect + "% accuracy!";
+                document.getElementById('outputDisplay').innerHTML = htmlOutput; //sends this string to the html file to print in a single box, at least for now. later will
+
+  }       catch (error) {
+        console.error("An error occurred while fetching or processing data:", error);
+        document.getElementById('outputDisplay').innerHTML = "Error: " + error.message;
+    }    });
+
+
+function calcPercent(redAmt, blueAmt, allianceColor) {
+    var scoreDifference;
+    if(allianceColor === ("red")) {
+        scoreDifference = redAmt - blueAmt;
+    } else {
+        scoreDifference = blueAmt - redAmt;
+    }
+    var calculatedPct = 1.0/(1.0+(Math.pow(10.0, (-scoreDifference/200.0))));
+    calculatedPct = Math.round(calculatedPct * 10000.0)/100.0;
+    return calculatedPct;
+}
+
