c72cfd5208742609c24038be9811ae9b45afc9f7
Lucas K. <lucas81610@gmail.com>
2026-06-15 16:30:22 -0400

    Create sw.js
    Required for web app to function

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'frc-predict-v2';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './sim.html',
+  './style.css', 
+  './javascript.js',
+  './manifest.json',
+  './icon192.png',
+  './icon512.png'
+  // Note: Add anything inside your /api folder if it contains local static JSON you need offline
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching app shell');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('Clearing old cache');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (!event.request.url.startsWith(self.location.origin)) return;
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
